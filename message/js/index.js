78fcba04b18734eeef7f5136f11a6a989748f927
apiiuw <2210501029@mahasiswa.upnvj.ac.id>
2024-10-27 00:26:05 +0700

    update

--- message/js/index.js
+++ message/js/index.js
@@ -1,6 +1,6 @@
 this.$dom = {};
 this.$dom.animatedText = document.createElement('div');
-this.text = "Banyak waktu aku lewatin dengan beribu harapan yang selalu pupus, akhirnya aku ketemu seseorang yang memenuhi semua harapan aku. Satu dari ribuan hal kecil yang aku pengen dari dulu adalah punya sebuah buku yang isinya tentang hubungan aku sama seseorang yang selalu aku pengen dari dulu. Aku orang yang gampang banget nethink, ketemu kamu yang selalu ngeredam nethink aku itu salah satu hal yang paling aku syukurin sampe sekarang. I have something for u, klik for next!!!";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.text = "Banyak waktu aku lewatin dengan beribu harapan yang selalu pupus, akhirnya aku ketemu seseorang yang memenuhi semua harapan aku. Satu dari ribuan hal kecil yang aku pengen dari dulu adalah punya sebuah buku yang isinya tentang hubungan aku sama seseorang yang selalu aku pengen dari dulu. Aku orang yang gampang banget nethink, ketemu kamu yang selalu ngeredam nethink aku itu salah satu hal yang paling aku syukurin sampe sekarang. I have something for u, klik for next!!!"; 
 this.$dom.container = document.getElementById('container');
 this.$dom.container.appendChild(this.$dom.animatedText);
 animateText();
